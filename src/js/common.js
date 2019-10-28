381aa686ebd1d6f9e977f5ddec3b12c36eaf01f5
Oleg Novikov <o.novikov@ivideon.com>
2019-10-28 20:09:11 +0300

    Transfer completed

--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,15 @@
+var loader;
+
 document.addEventListener('DOMContentLoaded', function () {
-    document.documentElement.insertBefore(document.querySelector('.' + $pageRootClassName + '__shadows'), document.body.nextSibling);
+    document.querySelector('#jsEmail').addEventListener('click', function (e) {
+        e.preventDefault();
+        window.location.href = 'mailto:olegn@outlook.com';
+    }, false);
+
+    loader = pageLoader();
+    document.documentElement.appendChild(loader);
+}, window.passiveSupported ? { passive: true } : false);
+
+window.addEventListener('load', function () {
+    loader.parentNode.removeChild(loader);
 }, window.passiveSupported ? { passive: true } : false);
